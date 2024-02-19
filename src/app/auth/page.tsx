60aa2591a802443f4f93ddeb26a73ce4a5961060
Pahreza Iqbal Prastowo <73816062+paresiqbal@users.noreply.github.com>
2024-02-19 14:13:17 +0700

    Add card componetns add form components add zod add button add input

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+// next
+import { useRouter } from "next/navigation";
+
+// zod
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+// shadcn
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const loginSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+  password: z.string().min(6, {
+    message: "Password must be at least 6 characters.",
+  }),
+});
+
+export default function Login() {
+  const router = useRouter();
+  return <div></div>;
+}
