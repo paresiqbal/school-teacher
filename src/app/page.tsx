2515aec95f5d7fed4a0fc87de523f65ccd98e2c8
Pahreza Iqbal Prastowo <73816062+paresiqbal@users.noreply.github.com>
2024-02-19 13:18:21 +0700

    add example image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
+// next
+import Image from "next/image";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Wellcome to School-app</h1>
+      <h1>SMK Negeri 1 Rejang Lebong</h1>
+      <div>
+        <h2>Manage Students Attendance and Grade's</h2>
+        <input type="text" />
+        <p>Contact admin @admin</p>
+      </div>
+      <div>
+        <Image
+          src="/placeholder.png"
+          width={500}
+          height={500}
+          priority
+          style={{ width: "auto", height: "100%" }}
+          alt="Picture of the author"
+        />
+      </div>
     </main>
   );
 }
