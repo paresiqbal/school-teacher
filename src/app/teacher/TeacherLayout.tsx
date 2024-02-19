34555d8a7bf6b15662247e6ff65bdaec1328cbf5
Pahreza Iqbal Prastowo <73816062+paresiqbal@users.noreply.github.com>
2024-02-19 23:13:57 +0700

    Add teacher layout

---
+++ src/app/teacher/TeacherLayout.tsx
@@ -0,0 +1,15 @@
+// components
+import Navbar from "@/components/Navbar";
+
+export default function TeacherLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <Navbar />
+      {children}
+    </>
+  );
+}
