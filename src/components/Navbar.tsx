e2cdd53a60b966ca0953415c70ff5b7203de1b36
Pahreza Iqbal Prastowo <73816062+paresiqbal@users.noreply.github.com>
2024-02-19 23:29:37 +0700

    asdasd

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -19,6 +19,22 @@ export default function Navbar() {
               className="w-48 md:w-64 lg:w-72"
             />
           </Link>
+          <button id="menu">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+              />
+            </svg>
+          </button>
         </div>
       </nav>
     </div>
