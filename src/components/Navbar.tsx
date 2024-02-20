899b64ce735fec2ac7472ac9df1c742d89c57911
Pahreza Iqbal Prastowo <73816062+paresiqbal@users.noreply.github.com>
2024-02-20 23:54:00 +0700

    asdasd

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,10 +12,10 @@ export default function Navbar() {
 
   return (
     <div className="max-w-[2000px] mx-auto">
-      <nav className="mx-auto p-4 bg-amber-400">
+      <nav className="mx-auto p-4 bg-zinc-900">
         <div className="mx-auto flex items-center gap-4">
           <button
-            className="lg:hidden focus:outline-none focus-visible:ring-4 ring-neutral-900 rounded-sm ring-offset-4 ring-offset-amber-400 text-neutral-900 hover:text-neutral-600 transition-colors"
+            className="lg:hidden focus:outline-none focus-visible:ring-4 ring-neutral-900 rounded-sm ring-offset-4 ring-offset-zinc-900 text-neutral-900 hover:text-neutral-600 transition-colors"
             onClick={toggleNavbar} // Attach the toggle function
           >
             <svg
@@ -37,7 +37,7 @@ export default function Navbar() {
             <img src="/logo.png" alt="logo" className="w-20 md:w-64 lg:w-72" />
           </Link>
           <div
-            className={`lg:flex lg:items-center lg:gap-4 transform top-0 left-0 w-[70%] lg:w-auto bg-amber-400 fixed lg:static h-full lg:h-auto overflow-auto ease-in-out transition-all duration-300 z-30 ${
+            className={`lg:flex lg:items-center lg:gap-4 transform top-0 left-0 w-[70%] lg:w-auto bg-zinc-900 fixed lg:static h-full lg:h-auto overflow-auto ease-in-out transition-all duration-300 z-30 ${
               isOpen ? "translate-x-0" : "-translate-x-full lg:translate-x-0"
             }`}
           >
@@ -64,7 +64,7 @@ export default function Navbar() {
             <div className="p-6 lg:p-0 lg:flex lg:items-center lg:gap-4">
               <Link
                 href="/"
-                className="block py-1 px-6 dark:text-neutral-900 focus:outline-none focus-visible:ring-4 ring-neutral-900 rounded-sm ring-offset-4 ring-offset-amber-400 hover:text-neutral-600 transition-colors"
+                className="block py-1 px-6 focus:outline-none focus-visible:ring-4 ring-neutral-900 rounded-sm ring-offset-4 ring-offset-zinc-900 transition-colors"
               >
                 Home
               </Link>
