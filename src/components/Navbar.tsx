d257b63c1eb8d01c5d422310f3d978d0b3a2781a
Pahreza Iqbal Prastowo <73816062+paresiqbal@users.noreply.github.com>
2024-02-20 21:57:46 +0700

    asdsad

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -21,18 +21,18 @@ export default function Navbar() {
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
-                stroke-width="1.5"
+                strokeWidth="1.5"
                 stroke="currentColor"
                 className="w-8 h-8"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                   d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
                 />
               </svg>
             </button>
-            <a
+            <Link
               href="/teacher"
               aria-label="go to homepage"
               className="z-50 md:w-32 lg:w-40"
@@ -43,8 +43,25 @@ export default function Navbar() {
                 width={200}
                 className="w-20 md:w-64 lg:w-72"
               />
-            </a>
+            </Link>
           </NavigationMenuItem>
+          <NavigationMenuList className="flex flex-col gap-4 absolute right-0 left-0 top-2">
+            <NavigationMenuItem>
+              <Link href="/teacher">Home</Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <Link href="/teacher/attendance">Attendance</Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <Link href="/teacher/grade">Grade</Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <Link href="/teacher/schedule">Schedule</Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <Link href="/teacher/announcement">Announcement</Link>
+            </NavigationMenuItem>
+          </NavigationMenuList>
         </NavigationMenuList>
       </NavigationMenu>
     </div>
