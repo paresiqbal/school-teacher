9e3710c992a54a97c2f74d2f9ba364192410fac0
Pahreza Iqbal Prastowo <73816062+paresiqbal@users.noreply.github.com>
2024-02-22 01:07:37 +0700

    asdasd

--- src/app/admin/student/page.tsx
+++ src/app/admin/student/page.tsx
@@ -1,49 +1,13 @@
-// shadcn
+"use client";
+import { useState } from "react";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
 
 export default function Student() {
+  const [showForm, setShowForm] = useState(false);
+
   return (
-    <div>
-      <Popover>
-        <PopoverTrigger asChild>
-          <Button variant="outline">Add New Student +</Button>
-        </PopoverTrigger>
-        <PopoverContent className="w-80">
-          <Card>
-            <CardHeader>
-              <CardTitle className="text-2xl lg:text-3xl font-bold">
-                Sign Up
-              </CardTitle>
-              <CardDescription className="text-sm">
-                Enter your username and password below to sign up.
-              </CardDescription>
-            </CardHeader>
-            <CardContent></CardContent>
-          </Card>
-        </PopoverContent>
-      </Popover>
+    <div className="space-y-4">
+      <Button onClick={() => setShowForm(!showForm)}>Add new student +</Button>
     </div>
   );
 }
