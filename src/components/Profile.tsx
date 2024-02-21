e19f76902f11329eae37f1bada65193ecce75965
Pahreza Iqbal Prastowo <73816062+paresiqbal@users.noreply.github.com>
2024-02-21 23:55:04 +0700

    asdasd

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -16,14 +16,13 @@ export default function Profile() {
         <DropdownMenuTrigger>
           <Avatar>
             <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-            <AvatarFallback>CN</AvatarFallback>
+            <AvatarFallback>P</AvatarFallback>
           </Avatar>
         </DropdownMenuTrigger>
         <DropdownMenuContent className="w-56">
           <DropdownMenuLabel>Super Admin</DropdownMenuLabel>
           <DropdownMenuSeparator />
           <DropdownMenuItem>Profile</DropdownMenuItem>
-
           <DropdownMenuItem>Logout</DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
