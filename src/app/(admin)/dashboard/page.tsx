2b64a880b58483344a1220985fae90ccb9e311bd
Pahreza Iqbal Prastowo <73816062+paresiqbal@users.noreply.github.com>
2024-02-29 01:28:43 +0700

    asdas

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,4 +1,19 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function Dashboard() {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (status === "unauthenticated") {
+      router.push("/login");
+    }
+  }, [router, status]);
+
   return (
     <div className="text-center p-10">
       <h1 className="text-2xl">Welcome to Admin Dashboard</h1>
--- src/app/api/auth/login/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextResponse, NextRequest } from "next/server";
-
-export async function POST(request: NextRequest) {
-  console.log(request);
-  return NextResponse.json({ status: 200, message: "Success" });
-}
