7f5ba6efa70f49cd705324160ec51d2416567581
Pahreza Iqbal Prastowo <73816062+paresiqbal@users.noreply.github.com>
2024-02-29 01:39:01 +0700

    create nextauth config

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,67 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const authOptions: NextAuthOptions = {
+  session: {
+    strategy: "jwt",
+  },
+  secret: "secret", // TODO: Change this to a secure secret
+  providers: [
+    CredentialsProvider({
+      type: "credentials",
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "Username" },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "*****",
+        },
+      },
+      async authorize(credentials) {
+        const { username, password } = credentials as {
+          username: string;
+          password: string;
+        };
+
+        const user: any = {
+          id: 1,
+          fullname: "Admin",
+          username: "admin",
+          role: "admin",
+        };
+
+        if (username === "admin" && password === "admin") {
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, account, profile, user }: any) {
+      if (account?.providers === "credentials") {
+        token.username = user.username;
+        token.fullname = user.fullname;
+        token.role = user.role;
+      }
+
+      return token;
+    },
+
+    async session({ session, token }: any) {
+      if ("username" in token) {
+        session.user.username = token.username;
+      }
+      if ("fullname" in token) {
+        session.user.fullname = token.fullname;
+      }
+      if ("role" in token) {
+        session.user.role = token.role;
+      }
+
+      return session;
+    },
+  },
+};
