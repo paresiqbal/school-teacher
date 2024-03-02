a8a6ab2431ba0ddc5d34c44fc3a435c1582c085d
Pahreza Iqbal Prastowo <73816062+paresiqbal@users.noreply.github.com>
2024-03-03 00:04:09 +0700

    asdasd

--- src/app/(admin)/teacher/[slug]/page.tsx
+++ src/app/(admin)/teacher/[slug]/page.tsx
@@ -1,11 +1,3 @@
-type DetailTeacherPageProps = { params: { slug: string } };
-
-export default function DetailTeacherPage({ params }: DetailTeacherPageProps) {
-  console.log(params);
-
-  return (
-    <div>
-      <h1>{params.slug}</h1>
-    </div>
-  );
+export default function page() {
+  return <div>page</div>;
 }
