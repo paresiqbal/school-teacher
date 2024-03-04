b1c4d3e73eaa35f6dd7f124f1f6dabc5c4c4a2e5
Pahreza Iqbal Prastowo <73816062+paresiqbal@users.noreply.github.com>
2024-03-04 23:31:52 +0700

    Add avatar and buttons to teacher page

--- src/app/(admin)/teacher/page.tsx
+++ src/app/(admin)/teacher/page.tsx
@@ -4,6 +4,8 @@ import TeacherRegister from "./TeacherRegister";
 
 // shadcn
 import { Card } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 interface Teacher {
   _id: number;
@@ -27,17 +29,38 @@ export default async function TeacherPage() {
   const teachers = await getTeachersData();
   return (
     <div className="p-10">
-      <h1 className="text-3xl font-bold">Teacher management</h1>
-      <p>create and manage teacher data</p>
+      <div>
+        <h1 className="text-3xl font-bold">Teacher management</h1>
+        <p>create and manage teacher data</p>
+      </div>
       <Card className="my-5 bg-card">
         <TeacherRegister />
       </Card>
       <Card className="rounded-xl border p-5 mx-auto my-5 bg-card text-card-foreground shadow col-span-3">
         {teachers.map((teacher) => (
-          <div key={teacher._id}>
-            <h2>{teacher.username}</h2>
-            <h3>{teacher.fullname}</h3>
-            <Link href={`/teacher/${teacher._id}`}>Edit</Link>
+          <div key={teacher._id} className="flex justify-between">
+            <div className="flex gap-2 items-center">
+              <Avatar>
+                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+              <div className="flex flex-col">
+                <p className="text-sm font-medium leading-none">
+                  {teacher.username}
+                </p>
+                <p className="text-sm text-muted-foreground">
+                  {teacher.fullname}
+                </p>
+              </div>
+            </div>
+            <div className="flex gap-2">
+              <Button>
+                <Link href={`/teacher/${teacher._id}`}>Edit</Link>
+              </Button>
+              <Button variant="destructive">
+                <Link href={"#"}>Delete</Link>
+              </Button>
+            </div>
           </div>
         ))}
       </Card>
