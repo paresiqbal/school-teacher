4272a6b943c841d475b1411580ed20076c52617e
Pahreza Iqbal Prastowo <73816062+paresiqbal@users.noreply.github.com>
2024-03-08 23:55:22 +0700

    asdasd

--- src/app/(admin)/student/[id]/page.tsx
+++ src/app/(admin)/student/[id]/page.tsx
@@ -7,6 +7,8 @@ import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 interface IStudent {
   username: string;
@@ -91,8 +93,71 @@ export default function StudentDetails({ params }: { params: Iid }) {
   }, [params.id]);
 
   return (
-    <div>
-      <h1></h1>
+    <div className="p-10">
+      <div>
+        <h1 className="text-2xl font-bold tracking-tight">Setting</h1>
+        <p className="text-muted-foreground">
+          Manage your account settings and set e-mail preferences.
+        </p>
+      </div>
+      <div className="flex gap-10 py-10">
+        <div className="inline-flex items-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:text-accent-foreground h-9 px-20 py-2 bg-muted hover:bg-muted justify-start">
+          Profile
+        </div>
+        {student ? (
+          <div className="flex flex-col w-full">
+            <h3 className="text-lg font-medium">Profile</h3>
+            <p className="text-sm text-muted-foreground">
+              This is how others will see you on the site
+            </p>
+            <form onSubmit={handleSubmit} className="py-8">
+              <label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+                Full Name:
+              </label>
+              <Input
+                type="text"
+                name="fullname"
+                value={formData.fullname}
+                onChange={handleInputChange}
+              />
+              <br />
+              <label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+                Username:
+                <Input
+                  type="text"
+                  name="username"
+                  value={formData.username}
+                  onChange={handleInputChange}
+                />
+              </label>
+              <br />
+              <label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+                Password:
+                <Input
+                  type="password"
+                  name="password"
+                  value={formData.password}
+                  onChange={handleInputChange}
+                />
+              </label>
+              <br />
+              <label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+                NIS
+                <Input
+                  type="number"
+                  name="nip"
+                  value={formData.nis}
+                  onChange={handleInputChange}
+                />
+              </label>
+              <br />
+              <Button type="submit">Update Profile</Button>
+            </form>
+          </div>
+        ) : (
+          <p>Loading teacher details...</p>
+        )}
+      </div>
     </div>
   );
 }
