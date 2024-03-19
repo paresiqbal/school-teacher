22363c1995c025ea2f7acdd8e3f5dcf58d57f884
Pahreza Iqbal Prastowo <73816062+paresiqbal@users.noreply.github.com>
2024-03-19 20:12:12 +0700

    Refactor level and major select elements in StudentList component

--- src/app/(admin)/student/StudentList.tsx
+++ src/app/(admin)/student/StudentList.tsx
@@ -106,37 +106,48 @@ export default function StudentList() {
 
   return (
     <div>
-      <label
-        htmlFor="level-select"
-        className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
-      >
-        Level:
-      </label>
-      <select
-        id="level-select"
-        className="bg-zinc-50 border border-yellow-400 text-white text-sm rounded-lg focus:ring-yellow-500 focus:border-yellow-500 w-52 p-2.5"
-        value={selectedLevel}
-        onChange={(e) => setSelectedLevel(e.target.value)}
-      >
-        <option value="">All Levels</option>
-        <option value="X">X</option>
-        <option value="XI">XI</option>
-        <option value="XII">XII</option>
-      </select>
-
-      <label htmlFor="major-select">Major:</label>
-      <select
-        id="major-select"
-        value={selectedMajor}
-        onChange={(e) => setSelectedMajor(e.target.value)}
-      >
-        <option value="">All Majors</option>
-        {majors.map((major) => (
-          <option key={major._id} value={major.majorName}>
-            {major.majorName}
-          </option>
-        ))}
-      </select>
+      <div className="flex gap-10 items-center py-2">
+        <div>
+          <label
+            htmlFor="level-select"
+            className="block mb-2 text-sm font-medium text-gray-400"
+          >
+            Level:
+          </label>
+          <select
+            id="level-select"
+            className="bg-zinc-900 border border-yellow-400 text-white text-sm rounded-lg focus:ring-yellow-500 focus:border-yellow-500 w-52 p-3"
+            value={selectedLevel}
+            onChange={(e) => setSelectedLevel(e.target.value)}
+          >
+            <option value="">All Levels</option>
+            <option value="X">X</option>
+            <option value="XI">XI</option>
+            <option value="XII">XII</option>
+          </select>
+        </div>
+        <div>
+          <label
+            htmlFor="major-select"
+            className="block mb-2 text-sm font-medium text-gray-400"
+          >
+            Major:
+          </label>
+          <select
+            id="major-select"
+            className="bg-zinc-900 border border-yellow-400 text-white text-sm rounded-lg focus:ring-yellow-500 focus:border-yellow-500 w-52 p-3"
+            value={selectedMajor}
+            onChange={(e) => setSelectedMajor(e.target.value)}
+          >
+            <option value="">All Majors</option>
+            {majors.map((major) => (
+              <option key={major._id} value={major.majorName}>
+                {major.majorName}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
       <Table>
         <TableCaption>A list of students.</TableCaption>
         <TableHeader>
