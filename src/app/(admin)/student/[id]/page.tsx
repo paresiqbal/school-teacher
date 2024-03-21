c2548e5a250d2b7a38398d700c872e87ebac55c3
Pahreza Iqbal Prastowo <73816062+paresiqbal@users.noreply.github.com>
2024-03-21 13:11:30 +0700

    Add @radix-ui/react-tabs package and implement Tabs component

--- src/app/(admin)/student/[id]/page.tsx
+++ src/app/(admin)/student/[id]/page.tsx
@@ -2,6 +2,7 @@
 
 // next
 import { ChangeEvent, FormEvent, useEffect, useState } from "react";
+import Image from "next/image";
 
 // library
 import { z } from "zod";
@@ -10,7 +11,8 @@ import QRCode from "qrcode";
 // shadcn
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { Separator } from "@/components/ui/separator";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 interface IStudent {
   username: string;
@@ -83,14 +85,13 @@ export default function StudentDetails({ params }: { params: Iid }) {
     e.preventDefault();
 
     try {
-      // Validate formData using Zod schema
       const validatedData = studentSchema.parse(formData);
 
       // Assuming the backend handles password hashing
       if (student) {
         await updateStudentDetail(params.id, {
           ...validatedData,
-          password: formData.password, // Ensure this is the new password the user wants to set
+          password: formData.password,
         });
         alert("Data updated successfully.");
       }
@@ -118,80 +119,91 @@ export default function StudentDetails({ params }: { params: Iid }) {
 
   return (
     <div className="p-10">
-      <div>
+      <div className="pb-10">
         <h1 className="text-2xl font-bold tracking-tight">Setting</h1>
         <p className="text-muted-foreground">
-          Manage your account settings and set e-mail preferences.
+          Manage your account settings and set user preferences.
         </p>
+        <Separator className="my-4" />
       </div>
-      <div className="flex gap-10 py-10">
-        <div className="inline-flex items-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:text-accent-foreground h-9 px-20 py-2 bg-muted hover:bg-muted justify-start">
-          Profile
-        </div>
-        {student ? (
-          <div className="flex flex-col w-full">
-            <h3 className="text-lg font-medium">Profile</h3>
-            <p className="text-sm text-muted-foreground">
-              This is how others will see you on the site
-            </p>
-            <form onSubmit={handleSubmit} className="py-8">
-              <label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
-                Full Name:
-              </label>
-              <Input
-                type="text"
-                name="fullname"
-                value={formData.fullname}
-                onChange={handleInputChange}
-              />
-              <br />
-              <label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
-                Username:
+      <Tabs defaultValue="account" className="w-full">
+        <TabsList className="grid w-full grid-cols-3 col-span-3">
+          <TabsTrigger value="Profile">Profile</TabsTrigger>
+          <TabsTrigger value="qrcode">QR Code</TabsTrigger>
+          <TabsTrigger value="password">Delete</TabsTrigger>
+        </TabsList>
+
+        <TabsContent value="Profile">
+          {student ? (
+            <div className="flex flex-col w-full py-4">
+              <h3 className="text-lg font-medium">Profile</h3>
+              <p className="text-sm text-muted-foreground">
+                This is how others will see you on the site
+              </p>
+              <form onSubmit={handleSubmit} className="py-8">
+                <label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+                  Full Name:
+                </label>
                 <Input
                   type="text"
-                  name="username"
-                  value={formData.username}
+                  name="fullname"
+                  value={formData.fullname}
                   onChange={handleInputChange}
                 />
-              </label>
-              <br />
-              <label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
-                Password:
-                <Input
-                  type="password"
-                  name="password"
-                  value={formData.password}
-                  onChange={handleInputChange}
-                />
-              </label>
-              <br />
-              <label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
-                NIS
-                <Input
-                  type="number"
-                  name="nip"
-                  value={formData.nis}
-                  onChange={handleInputChange}
-                />
-              </label>
-              <br />
-              <Button type="submit">Update Profile</Button>
-              <Button onClick={handleGenerateQRCode}>Generate QR Code</Button>
-              {qrCodeDataURL && (
-                <Image
-                  width={500}
-                  height={500}
-                  className="w-32 h-32 rounded-md overflow-hidden"
-                  src={qrCodeDataURL}
-                  alt="QR Code"
-                />
-              )}
-            </form>
-          </div>
-        ) : (
-          <p>Loading teacher details...</p>
-        )}
-      </div>
+                <br />
+                <label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+                  Username:
+                  <Input
+                    type="text"
+                    name="username"
+                    value={formData.username}
+                    onChange={handleInputChange}
+                  />
+                </label>
+                <br />
+                <label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+                  Password:
+                  <Input
+                    type="password"
+                    name="password"
+                    value={formData.password}
+                    onChange={handleInputChange}
+                  />
+                </label>
+                <br />
+                <label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+                  NIS
+                  <Input
+                    type="number"
+                    name="nip"
+                    value={formData.nis}
+                    onChange={handleInputChange}
+                  />
+                </label>
+                <br />
+                <Button type="submit">Update Profile</Button>
+                {/* <Button onClick={handleGenerateQRCode}>Generate QR Code</Button>
+                {qrCodeDataURL && (
+                  <div>
+                    <Image
+                      width={500}
+                      height={500}
+                      className="w-32 h-32 rounded-md overflow-hidden"
+                      src={qrCodeDataURL}
+                      alt="QR Code"
+                    />
+                    <p>Nama</p>
+                  </div>
+                )} */}
+              </form>
+            </div>
+          ) : (
+            <p>Loading student details...</p>
+          )}
+        </TabsContent>
+        <TabsContent value="Qrcode"></TabsContent>
+        <TabsContent value="Delete"></TabsContent>
+      </Tabs>
     </div>
   );
 }
