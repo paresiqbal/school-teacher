12a189641e3b72073c76059fed8c5c0c3e3e869f
Pahreza Iqbal Prastowo <73816062+paresiqbal@users.noreply.github.com>
2024-03-26 07:21:04 +0700

    Add html2canvas library for generating QR code images

--- src/app/(admin)/student/[id]/QrForm.tsx
+++ src/app/(admin)/student/[id]/QrForm.tsx
@@ -2,11 +2,11 @@
 import { useState, useEffect } from "react";
 import Image from "next/image";
 import QRCode from "qrcode";
-import { Input } from "@/components/ui/input";
+import html2canvas from "html2canvas";
 import { Button } from "@/components/ui/button";
 
 type Student = {
-  id: string;
+  _id: string;
   nis: string;
   fullname: string;
   major: string;
@@ -24,11 +24,8 @@ export default function QrForm({ id }: { id: string }) {
           `http://localhost:3001/student/student/${id}`
         );
         if (!response.ok) throw new Error("Failed to fetch student data");
-
-        // Ensure there's data to parse
         const text = await response.text();
         if (!text) throw new Error("No data returned from fetch");
-
         const data: Student = JSON.parse(text);
         setStudent(data);
       } catch (error) {
@@ -43,12 +40,26 @@ export default function QrForm({ id }: { id: string }) {
   const handleGenerateQrCode = async () => {
     try {
       if (!student) return;
-      const qrCodeData = `Student ID: ${student.id}, Name: ${student.fullname}`;
+      const qrCodeData = `Student ID: ${student._id}, Name: ${student.fullname}, NIS: ${student.nis}`;
       const qrUrl = await QRCode.toDataURL(qrCodeData);
       setQrCodeUrl(qrUrl);
     } catch (error) {
       console.error("Error generating QR code:", error);
       setError("Failed to generate QR code");
+    }
+  };
+
+  const handleDownloadImage = async () => {
+    const content = document.getElementById("qr-code-container");
+    if (content) {
+      const canvas = await html2canvas(content);
+      const image = canvas.toDataURL("image/png");
+      const link = document.createElement("a");
+      link.href = image;
+      link.download = "student-qr-code.png";
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
     }
   };
 
@@ -64,29 +75,35 @@ export default function QrForm({ id }: { id: string }) {
         {error && <p className="text-red-500">Error: {error}</p>}
         {student && (
           <div>
-            <h2 className="text-xl font-semibold mb-4">Student Details</h2>
             <p className="text-lg">
-              Student Name:{" "}
-              <span className="font-medium">{student.fullname}</span>
+              ID: <span className="font-medium">{student._id}</span>
+            </p>
+            <p className="text-lg">
+              NIS: <span className="font-medium">{student.nis}</span>
+            </p>
+            <p className="text-lg">
+              Name: <span className="font-medium">{student.fullname}</span>
             </p>
             <p className="text-lg">
               Major: <span className="font-medium">{student.major}</span>
             </p>
-            <Button onClick={handleGenerateQrCode}>Generate QR Code</Button>
+            <div className="flex flex-col gap-2">
+              <Button onClick={handleGenerateQrCode}>Generate QR Code</Button>
+              <Button onClick={handleDownloadImage}>Download QR</Button>
+            </div>
           </div>
         )}
         {qrCodeUrl && (
-          <div className="text-center">
-            <p className="text-xl font-semibold mb-2">QR Code:</p>
-            <Image src={qrCodeUrl} alt="QR Code" width={300} height={300} />
-            <p className="text-lg mt-4">{student?.fullname}</p>
-            <a
-              href={qrCodeUrl}
-              download={`${student?.fullname}-QRCode.png`}
-              className="inline-block mt-4 bg-yellow-400 hover:bg-yellow-500 text-zinc-950 font-bold py-2 px-4 rounded"
-            >
-              Download QR Code
-            </a>
+          <div
+            id="qr-code-container"
+            className="text-center bg-white rounded-md p-2"
+          >
+            <div className="text-center">
+              <Image src={qrCodeUrl} alt="QR Code" width={250} height={250} />
+              <p className="text-2xl font-bold text-zinc-900">
+                {student?.fullname}
+              </p>
+            </div>
           </div>
         )}
       </div>
