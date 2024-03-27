ee203eae57acdbcac43a01ef4092baed14e4eb16
Pahreza Iqbal Prastowo <73816062+paresiqbal@users.noreply.github.com>
2024-03-28 05:40:51 +0700

    Add data fetching and display attendance records

--- src/app/(admin)/(attendance)/teacherRecord/[id]/page.tsx
+++ src/app/(admin)/(attendance)/teacherRecord/[id]/page.tsx
@@ -1,13 +1,74 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 interface Iid {
   id: string;
 }
 
+interface Student {
+  _id: string;
+  fullname: string;
+  isPresent: boolean;
+}
+
+interface AttendanceRecord {
+  _id: string;
+  subject: string;
+  date: string;
+  students: Student[];
+}
+
+interface FetchDataResponse {
+  attendanceRecords: AttendanceRecord[];
+}
+
+const fetchData = async (
+  id: string
+): Promise<FetchDataResponse | undefined> => {
+  const params = new URLSearchParams({ teacherId: id });
+  const url = `http://localhost:3001/attendance/attendance-teacher/?${params.toString()}`;
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = (await response.json()) as FetchDataResponse;
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 export default function DetailAttendance({ params }: { params: Iid }) {
-  const { id } = params;
+  const [data, setData] = useState<FetchDataResponse | null>(null);
+
+  useEffect(() => {
+    fetchData(params.id).then((data) => {
+      if (data !== undefined) {
+        setData(data);
+      }
+    });
+  }, [params.id]);
 
   return (
     <div>
-      <h1>sasd</h1>
+      <h1>{data ? "Attendance Records" : "Loading..."}</h1>
+      {data?.attendanceRecords.map((record) => (
+        <div key={record._id}>
+          <h2>
+            {record.subject} - {new Date(record.date).toLocaleDateString()}
+          </h2>
+          {/* <ul>
+            {record.students.map((student) => (
+              <li key={student._id}>
+                {student.fullname}: {student.isPresent}
+              </li>
+            ))}
+          </ul> */}
+        </div>
+      ))}
     </div>
   );
 }
