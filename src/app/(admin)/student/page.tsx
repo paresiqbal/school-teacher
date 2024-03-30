414fe211103665cd93b2b871a9a700fe80e9a024
Pahreza Iqbal Prastowo <73816062+paresiqbal@users.noreply.github.com>
2024-03-30 12:42:06 +0700

    Update layout and add breadcrumb component

--- src/app/(admin)/student/page.tsx
+++ src/app/(admin)/student/page.tsx
@@ -4,11 +4,33 @@ import StudentList from "./StudentList";
 
 // shadcn
 import { Card } from "@/components/ui/card";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import Link from "next/link";
 
 export default function StudentPage() {
   return (
     <div className="p-10 bg-muted/40">
-      <div>
+      <Breadcrumb className="hidden md:flex">
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link href="/dashboard">Dashboard</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>Student</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+      <div className="pt-8">
         <h1 className="text-3xl font-bold">Student management</h1>
         <p>create and manage student data</p>
       </div>
