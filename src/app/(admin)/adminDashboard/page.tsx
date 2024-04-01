5de1d3db6ea9ae383ed4b29a0404e336acb342d4
Pahreza Iqbal Prastowo <73816062+paresiqbal@users.noreply.github.com>
2024-04-01 17:52:10 +0700

    Add next/navigation and useEffect to handle unauthenticated status

--- src/app/(admin)/adminDashboard/page.tsx
+++ src/app/(admin)/adminDashboard/page.tsx
@@ -1,11 +1,18 @@
 "use client";
 
 import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function AdminDashboard() {
   const { data: session, status } = useSession();
+  const router = useRouter();
 
-  console.log(session, status);
+  useEffect(() => {
+    if (status === "unauthenticated") {
+      router.push("/login");
+    }
+  }, [status, router]);
 
   return (
     <div className="text-center p-10 bg-muted/40">
