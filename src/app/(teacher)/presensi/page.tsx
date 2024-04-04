f7dbde8ecbc802b51242fe7c164a2d9dc9035b76
Pahreza Iqbal Prastowo <73816062+paresiqbal@users.noreply.github.com>
2024-04-05 06:53:51 +0700

    Refactor teacher pages and add Presensi link

--- src/app/(teacher)/presensi/page.tsx
+++ src/app/(teacher)/presensi/page.tsx
@@ -1,3 +1,18 @@
-export default function AboutPage() {
-  return <div>AboutPage</div>;
+"use client";
+
+import { useSession } from "next-auth/react";
+
+export default function Presensi() {
+  const { data: session, status }: { data: any; status: string } = useSession();
+  console.log(session);
+
+  return (
+    <div>
+      <div>
+        <h1>Presensi</h1>
+        <h2>{session?.user?.fullname}</h2>
+        <p>{session?.user?.id}</p>
+      </div>
+    </div>
+  );
 }
--- src/app/(teacher)/teacherDashboard/page.tsx
+++ src/app/(teacher)/teacherDashboard/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useSession } from "next-auth/react";
+import Link from "next/link";
 
 export default function TeacherDashboard() {
   const { data: session, status }: { data: any; status: string } = useSession();
@@ -11,6 +12,9 @@ export default function TeacherDashboard() {
       <h1>Teacher Dashboard</h1>
       <h2>{session?.user?.fullname}</h2>
       <p>{session?.user?.id}</p>
+      <div>
+        <Link href={"/presensi"}>Presensi</Link>
+      </div>
     </div>
   );
 }
