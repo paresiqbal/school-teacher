847bc64b7446136c81f67343ea111608db8fb678
Pahreza Iqbal Prastowo <73816062+paresiqbal@users.noreply.github.com>
2024-04-05 05:50:38 +0700

    Update page title and button text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ import { signIn } from "next-auth/react";
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center my-10 p-10 gap-20">
-      <h1 className="text-2xl lg:text-5xl font-bold text-center">
+      <h1 className="text-3xl lg:text-5xl font-bold text-center">
         SMK NEGERI 1 REJANG LEBONG
       </h1>
       <div className="flex flex-col lg:flex-row w-full items-center gap-8 lg:gap-20">
@@ -19,20 +19,20 @@ export default function Home() {
             src="/images/awal.svg"
             width={500}
             height={500}
-            priority
+            loading="lazy"
             alt="School image"
-            className="rounded-lg"
+            className="rounded-lg w-3/5 h-auto"
           />
         </div>
         <div className="flex flex-col gap-4 items-center lg:items-start lg:w-1/2">
           <h2 className="text-xl lg:text-3xl text-center lg:text-left">
-            Manage Students Attendance and Grades
+            Kelola data, kehadiran, dan nilai siswa.
           </h2>
           <Button className="w-full lg:w-auto" onClick={() => signIn()}>
-            Sign In
+            Masuk
           </Button>
           <p className="text-sm lg:text-base text-center lg:text-left">
-            Contact admin @superadmin
+            Hubungi Admin @superadmin
           </p>
         </div>
       </div>
