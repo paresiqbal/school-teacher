166820a3bebbc2e7704ddb198fd19f1c3613a1b3
Pahreza Iqbal Prastowo <73816062+paresiqbal@users.noreply.github.com>
2024-04-05 07:38:45 +0700

    Add presensi dialog component

--- src/app/(teacher)/presensi/page.tsx
+++ src/app/(teacher)/presensi/page.tsx
@@ -2,16 +2,55 @@
 
 import { useSession } from "next-auth/react";
 
+// shadcn
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
 export default function Presensi() {
   const { data: session, status }: { data: any; status: string } = useSession();
   console.log(session);
 
   return (
-    <div>
-      <div>
+    <div className="flex flex-col items-center justify-center">
+      <div className="text-center">
         <h1>Presensi</h1>
         <h2>{session?.user?.fullname}</h2>
-        <p>{session?.user?.id}</p>
+      </div>
+      <div className="py-10">
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="outline">Check Presensi</Button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>Subject</DialogTitle>
+              <DialogDescription>
+                Enter subject you teach right now.
+              </DialogDescription>
+            </DialogHeader>
+            <div className="grid gap-4 py-4">
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="subject" className="text-right">
+                  Name
+                </Label>
+                <Input id="subject" value="" className="col-span-3" />
+              </div>
+            </div>
+            <DialogFooter>
+              <Button type="submit">Submit</Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
       </div>
     </div>
   );
