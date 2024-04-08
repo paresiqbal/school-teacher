a116ffcbd57107166ad02dbff7bcfdfad30bdda5
Pahreza Iqbal Prastowo <73816062+paresiqbal@users.noreply.github.com>
2024-04-08 15:53:44 +0700

    Refactor teacher dashboard page layout and add button component

--- src/app/(teacher)/teacherDashboard/page.tsx
+++ src/app/(teacher)/teacherDashboard/page.tsx
@@ -1,19 +1,26 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
 
 export default function TeacherDashboard() {
-  const { data: session, status }: { data: any; status: string } = useSession();
-  console.log(session);
+  const { data: session }: { data: any } = useSession();
 
   return (
-    <div>
-      <h1>Teacher Dashboard</h1>
-      <h2>{session?.user?.fullname}</h2>
-      <p>{session?.user?.id}</p>
-      <div>
-        <Link href={"/presensi"}>Presensi</Link>
+    <div className="p-10">
+      <h1 className="text-4xl font-bold">Teacher Dashboard</h1>
+      <div className="py-6">
+        <h2>
+          Wellcome
+          <span className="text-yellow-500 font-bold">
+            {session?.user?.fullname}
+          </span>
+        </h2>
+
+        <Button>
+          <Link href={"/presensi"}>Go to Presensi</Link>
+        </Button>
       </div>
     </div>
   );
