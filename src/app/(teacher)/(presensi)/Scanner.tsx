408c292a95427cb31d443b6dddbd58aca35bec8a
Pahreza Iqbal Prastowo <73816062+paresiqbal@users.noreply.github.com>
2024-04-16 21:44:32 +0700

    asdas

--- src/app/(teacher)/(presensi)/Scanner.tsx
+++ src/app/(teacher)/(presensi)/Scanner.tsx
@@ -1,30 +1,3 @@
-import React, { useEffect, useRef } from "react";
-import { Html5QrcodeScanner } from "html5-qrcode";
-
-const Scanner: React.FC = () => {
-  const scannerRef = useRef<Html5QrcodeScanner | null>(null);
-
-  useEffect(() => {
-    const onScanSuccess = (decodedText: string, decodedResult: any) => {
-      console.log(`Code matched = ${decodedText}`, decodedResult);
-    };
-
-    const onScanFailure = (error: any) => {
-      console.warn(`Code scan error = ${error}`);
-    };
-
-    const config = { fps: 10, qrbox: { width: 250, height: 250 } };
-    const html5QrcodeScanner = new Html5QrcodeScanner("reader", config, false);
-    scannerRef.current = html5QrcodeScanner;
-
-    html5QrcodeScanner.render(onScanSuccess, onScanFailure);
-
-    return () => {
-      html5QrcodeScanner.clear().then(() => console.log("Scanner stopped."));
-    };
-  }, []);
-
-  return <div id="reader" style={{ width: "500px", height: "500px" }}></div>;
-};
-
-export default Scanner;
+export default function Scanner() {
+  return <div>Scanner</div>;
+}
