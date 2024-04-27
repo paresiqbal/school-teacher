51ee3b136b1ce36da4a23f251320bc44f5911e76
Pahreza Iqbal Prastowo <73816062+paresiqbal@users.noreply.github.com>
2024-04-27 13:02:40 +0700

    Refactor layout and comment out unused code

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -6,16 +6,11 @@ export default function AdminLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <head />
-      <body>
-        <div className="flex">
-          <div className="sticky top-0 h-screen">
-            <Sidebar />
-          </div>
-          <div className="w-full">{children}</div>
-        </div>
-      </body>
-    </html>
+    <div className="flex">
+      <div className="sticky top-0 h-screen">
+        <Sidebar />
+      </div>
+      <div className="w-full">{children}</div>
+    </div>
   );
 }
--- src/app/(admin)/student/page.tsx
+++ src/app/(admin)/student/page.tsx
@@ -36,11 +36,9 @@ export default function StudentPage() {
         <h1 className="text-3xl font-bold">Student management</h1>
         <p>create and manage student data</p>
       </div>
-      <Card className="my-5">
-        <StudentRegister />
-      </Card>
+      <Card className="my-5">{/* <StudentRegister /> */}</Card>
       <Card className="rounded-xl border p-5 mx-auto my-5 bg-card text-card-foreground shadow col-span-3">
-        <StudentList />
+        {/* <StudentList /> */}
       </Card>
     </div>
   );
