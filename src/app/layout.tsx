c84b3eeea4773fc24e63d92d44b5fec2eda87ac8
paresiqbal <thecrowdel@gmail.com>
2024-05-04 21:38:38 +0700

    chore: update default theme to "system" in RootLayout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
         <SessionProvider>
           <ThemeProvider
             attribute="class"
-            defaultTheme="dark"
+            defaultTheme="system"
             enableSystem
             disableTransitionOnChange
           >
